2187d6af225f0dbfe91b81be2651af94fbb944cd
George Castro <gamcastro@bol.com.br>
2023-05-05 03:56:03 -0300

    Realizado mudança estrutural

--- GeoFinanceiroUtils.psm1
+++ GeoFinanceiroUtils.psm1
@@ -7,46 +7,62 @@ function Get-GeoFaturaCartaoCredito {
         [ValidatePattern("\.txt$",ErrorMessage="Não é um arquivo .txt")]       
         [string]$FilePath,
 
-        [Parameter(Mandatory=$True)]
+       
         [string[]]$Filter
+
+        
     )
     BEGIN {
 
     }
     PROCESS {
+        $params = @{'Path' = $FilePath
+                    'Pattern' = "^\d{2}\.\d{2}\.\d{4}"
+                    'Encoding' = 'utf8'
+                    'Raw' = $True}
+        $padrao = "(?<=\d{2}\.\d{2}\.\d{4})(.+)(?=BR)"
+        select-string @params | ForEach-Object {
+            $descCompra = Select-String -InputObject $_ -Pattern $padrao | ForEach-Object { $_.Matches.Value.Trim() }
+            $valorItem = [decimal]::Parse(($_.Substring(62,7)),(Get-Culture))
+            $props = @{'ItemCompra' = $descCompra
+                        'ValorItem' = $valorItem}
+           $obj = New-Object -TypeName psobject -Property $props
+           Write-Output $obj
+        }
+        
         #busca no arquivo linha por linha o o que foi inserido no parametro $filter (Ex: 'UBER')
-        Write-Verbose "Consultando o arquivo $FilePath"
-        foreach ($filtro in $Filter) {
-            $params = @{'Path' = $FilePath
-                'Pattern'      = $filtro
-            }
+        # Write-Verbose "Consultando o arquivo $FilePath"
+        # foreach ($filtro in $Filter) {
+        #     $params = @{'Path' = $FilePath
+        #         'Pattern'      = $filtro
+        #     }
 
-            Write-Verbose "Processando o filtro $filtro"
-            $valortotal = 0
-            try {
-                Select-String  @params |
-                ForEach-Object {
+        #     Write-Verbose "Processando o filtro $filtro"
+        #     $valortotal = 0
+        #     try {
+        #         Select-String  @params |
+        #         ForEach-Object {
 
-                    # Busca a parte do valor na string e troca a virgula pelo ponto
-                    $valor = ($_.Line.Substring(62, 7)).Replace(",", ".")
-                    $valor_m = $valor -as [decimal]
-                    $valortotal = $valortotal + $valor_m
+        #             # Busca a parte do valor na string e troca a virgula pelo ponto
+        #             $valor = ($_.Line.Substring(62, 7)).Replace(",", ".")
+        #             $valor_m = $valor -as [decimal]
+        #             $valortotal = $valortotal + $valor_m
             
-                }
-                $props = @{'Categoria' = $filtro
-                    'Valor Total'      = $valortotal
-                }
+        #         }
+        #         $props = @{'Categoria' = $filtro
+        #             'Valor Total'      = $valortotal
+        #         }
 
-                $obj = New-Object -TypeName psobject -Property $props
+        #         $obj = New-Object -TypeName psobject -Property $props
 
                
-                Write-Output $obj
-            }
-            catch {
-                Write-Warning "Falha ao ler o arquivo $FilePath"
-            }
+        #         Write-Output $obj
+        #     }
+        #     catch {
+        #         Write-Warning "Falha ao ler o arquivo $FilePath"
+        #     }
             
-        }
+        # }
        
     }
     END {
