9309485f69c78326a640f9b0225745b89ec66711
George Castro <gamcastro@bol.com.br>
2023-05-06 17:55:03 -0300

    Reformulado o objeto de items de compra

--- GeoFinanceiroUtils.psm1
+++ GeoFinanceiroUtils.psm1
@@ -20,50 +20,19 @@ function Get-GeoFaturaCartaoCredito {
                     'Pattern' = "^\d{2}\.\d{2}\.\d{4}"
                     'Encoding' = 'utf8'
                     'Raw' = $True}
-        $padrao = "(?<=\d{2}\.\d{2}\.\d{4})(.+)(?=BR)"
+      
         select-string @params | ForEach-Object {
-            $descCompra = Select-String -InputObject $_ -Pattern $padrao | ForEach-Object { $_.Matches.Value.Trim() }
-            $valorItem = [decimal]::Parse(($_.Substring(62,7)),(Get-Culture))
+            $descCompra = $_.Substring(10,34)
+             $valorItem = [decimal]::Parse(($_.Substring(62,7)),(Get-Culture))
+             $dataDaCompra = [DateTime]::Parse(($_.Substring(0,10)),(Get-Culture))
             $props = @{'ItemCompra' = $descCompra
-                        'ValorItem' = $valorItem}
+                        'ValorItem' = $valorItem
+                        'DataDaCompra' = $dataDaCompra}
            $obj = New-Object -TypeName psobject -Property $props
            Write-Output $obj
         }
         
-        #busca no arquivo linha por linha o o que foi inserido no parametro $filter (Ex: 'UBER')
-        # Write-Verbose "Consultando o arquivo $FilePath"
-        # foreach ($filtro in $Filter) {
-        #     $params = @{'Path' = $FilePath
-        #         'Pattern'      = $filtro
-        #     }
-
-        #     Write-Verbose "Processando o filtro $filtro"
-        #     $valortotal = 0
-        #     try {
-        #         Select-String  @params |
-        #         ForEach-Object {
-
-        #             # Busca a parte do valor na string e troca a virgula pelo ponto
-        #             $valor = ($_.Line.Substring(62, 7)).Replace(",", ".")
-        #             $valor_m = $valor -as [decimal]
-        #             $valortotal = $valortotal + $valor_m
-            
-        #         }
-        #         $props = @{'Categoria' = $filtro
-        #             'Valor Total'      = $valortotal
-        #         }
-
-        #         $obj = New-Object -TypeName psobject -Property $props
-
-               
-        #         Write-Output $obj
-        #     }
-        #     catch {
-        #         Write-Warning "Falha ao ler o arquivo $FilePath"
-        #     }
-            
-        # }
-       
+           
     }
     END {
 
